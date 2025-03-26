c7e87a044ea0077c06fc3675056a85be9fc5b12b
Pixie <B00159841@mytudublin.ie>
2025-03-26 17:16:05 +0000

    Updated the read.php file

--- public/read.php
+++ public/read.php
@@ -1,9 +1,11 @@
-<?php include "templates/header.php"; ?>
-<h2>Find user based on location</h2>
-<form method="post">
-<label for="location">Location</label>
-<input type="text" id="location" name="location">
-<input type="submit" name="submit" value="View Results">
-</form>
-<a href="index.php">Back to home</a>
-<?php include "templates/footer.php"; ?>
+<?php
+if (isset($_POST['submit'])) {
+    try {
+        require "../common.php";
+        require_once '../src/DBconnect.php';
+        
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+}
+?>
