9d35d12e65a0379602791ae6b6cd7e5aa5beb75d
Pixie <B00159841@mytudublin.ie>
2025-03-26 17:19:33 +0000

    Updated read.php

--- public/read.php
+++ public/read.php
@@ -4,6 +4,18 @@ if (isset($_POST['submit'])) {
         require "../common.php";
         require_once '../src/DBconnect.php';
         
+        $sql = "SELECT *
+        FROM users
+        WHERE location = :location";
+        $location = $_POST['location'];
+        $statement = $connection->prepare($sql);
+        $statement->bindParam(':location', $location, PDO::PARAM_STR);
+        $statement->execute();
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+        
     } catch(PDOException $error) {
         echo $sql . "<br>" . $error->getMessage();
     }
