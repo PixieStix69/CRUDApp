c92980918a6975c855c05943666dcc9ad2dbb080
Pixie <B00159841@mytudublin.ie>
2025-03-26 17:46:33 +0000

    Updated update-single.php

--- public/update-single.php
+++ public/update-single.php
@@ -18,3 +18,14 @@ require "../common.php";
         exit;
     }
 ?>
+
+<?php require "templates/header.php"; ?>
+    <h2>Edit a user</h2>
+    <form method="post">
+<?php foreach ($user as $key => $value) : ?>
+
+<?php endforeach; ?>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+        <a href="index.php">Back to home</a>
+<?php require "templates/footer.php"; ?>
