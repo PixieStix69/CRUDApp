8e53797ebf2f62e562cffafb871b8a69f004e10b
Pixie <B00159841@mytudublin.ie>
2025-03-26 17:36:06 +0000

    Updated update.php

--- public/update.php
+++ public/update.php
@@ -1,10 +1,17 @@
 <?php
+/**
+* List all users with a link to edit
+*/
+    try {
+        require "../common.php";
+        require_once '../src/DBconnect.php';
+        $sql = "SELECT * FROM users";
+        $statement = $connection->prepare($sql);
+        $statement->execute();
+        $result = $statement->fetchAll();
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
 
-$sql = "SELECT * FROM users";
-
-$statement = $connection->prepare($sql);
-$statement->execute();
-
-$result = $statement->fetchAll();
-
+    
 ?>
