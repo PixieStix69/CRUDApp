433dffa6f6a0c590e0ef2420004ba36fb0e4adbf
Pixie <B00159841@mytudublin.ie>
2025-03-26 16:20:49 +0000

    Updating index.php (Adding the templates I made)

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
-    <ul>
-        <li>
-            <a href="create.php"><strong>Create</strong></a> - add a user
-        </li>
-        <li>
-            <a href="read.php"><strong>Read</strong></a> - find a user
-        </li>
-    </ul>
-</body>
-</html>
+<?php include "templates/header.php"; ?>
+<ul>
+<li>
+<a href="create.php"><strong>Create</strong></a> - add a user
+</li>
+<li>
+<a href="read.php"><strong>Read</strong></a> - find a user
+</li>
+</ul>
+<?php include "templates/footer.php"; ?>
