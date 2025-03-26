220a2f15a605045049edf868506258b2a4a2deed
Pixie <B00159841@mytudublin.ie>
2025-03-26 17:41:51 +0000

    Updated update.php

--- public/update.php
+++ public/update.php
@@ -12,8 +12,6 @@
     } catch(PDOException $error) {
         echo $sql . "<br>" . $error->getMessage();
     }
-
-
 ?>
 
 <?php require "templates/header.php"; ?>
@@ -28,22 +26,26 @@
     <th>Age</th>
     <th>Location</th>
     <th>Date</th>
+    <th>Edit</th>
     </tr>
     </thead>
     <tbody>
-    <?php foreach ($result as $row) : ?>
+<?php foreach ($result as $row) : ?>
     <tr>
-    <td><?php echo escape($row["id"]); ?></td>
-    <td><?php echo escape($row["firstname"]); ?></td>
-    <td><?php echo escape($row["lastname"]); ?></td>
-    <td><?php echo escape($row["email"]); ?></td>
-    <td><?php echo escape($row["age"]); ?></td>
-    <td><?php echo escape($row["location"]); ?></td>
-    <td><?php echo escape($row["date"]); ?> </td>
-    </tr>
-    <?php endforeach; ?>
-    </tbody>
-    </table>
-    <a href="index.php">Back to home</a>
+        <td><?php echo escape($row["id"]); ?></td>
+        <td><?php echo escape($row["firstname"]); ?></td>
+        <td><?php echo escape($row["lastname"]); ?></td>
+        <td><?php echo escape($row["email"]); ?></td>
+        <td><?php echo escape($row["age"]); ?></td>
+        <td><?php echo escape($row["location"]); ?></td>
+        <td><?php echo escape($row["date"]); ?> </td>
+        <td><a href="update-single.php?id=<?php echo escape($row["id"]);
+        ?>">Edit</a></td>
+        </tr>
 
-    
+<?php endforeach; ?>
+        </tbody>
+        </table>
+        <a href="index.php">Back to home</a>
+
+<?php require "templates/footer.php"; ?>
