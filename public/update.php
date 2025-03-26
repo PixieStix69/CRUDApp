295ac7a5a17413a01bec70247468bfe3651511f0
Pixie <B00159841@mytudublin.ie>
2025-03-26 17:37:46 +0000

    Updated update.php

--- public/update.php
+++ public/update.php
@@ -13,5 +13,37 @@
         echo $sql . "<br>" . $error->getMessage();
     }
 
-    
+
 ?>
+
+<?php require "templates/header.php"; ?>
+    <h2>Update users</h2>
+    <table>
+    <thead>
+    <tr>
+    <th>#</th>
+    <th>First Name</th>
+    <th>Last Name</th>
+    <th>Email Address</th>
+    <th>Age</th>
+    <th>Location</th>
+    <th>Date</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($result as $row) : ?>
+    <tr>
+    <td><?php echo escape($row["id"]); ?></td>
+    <td><?php echo escape($row["firstname"]); ?></td>
+    <td><?php echo escape($row["lastname"]); ?></td>
+    <td><?php echo escape($row["email"]); ?></td>
+    <td><?php echo escape($row["age"]); ?></td>
+    <td><?php echo escape($row["location"]); ?></td>
+    <td><?php echo escape($row["date"]); ?> </td>
+    </tr>
+    <?php endforeach; ?>
+    </tbody>
+    </table>
+    <a href="index.php">Back to home</a>
+
+    
