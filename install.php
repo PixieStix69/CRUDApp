7d21acd01dd7b4a32d83851b18b91cf8f3c021de
Pixie <B00159841@mytudublin.ie>
2025-03-26 16:50:03 +0000

    Updated install.php

--- install.php
+++ install.php
@@ -1,7 +1,16 @@
 <?php 
+/**
+* Open a connection via PDO to create a new database and table with
+* structure. */
 require "config.php";
-$connection = new PDO("mysql:host=$host", $username, $password, $options);
+try {
+$connection = new PDO("mysql:host=$host", $username, $password,
+$options);
 $sql = file_get_contents("data/init.sql");
 $connection->exec($sql);
-Echo "DB setup";
+echo "Database and table users created successfully.";
+} catch(PDOException $error) {
+echo $sql . "<br>" . $error->getMessage();
+}
+
 ?>
