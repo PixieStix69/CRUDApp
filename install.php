f24d03b7d0774300eab1dfe73bcb6256900e4bae
Pixie <B00159841@mytudublin.ie>
2025-03-26 16:54:34 +0000

    upodated structure of install.php

--- install.php
+++ install.php
@@ -1,12 +1,22 @@
 <?php 
+/**
+* Open a connection via PDO to create a new database and table with
+* structure. */
 if (isset($_POST['submit'])) {
-    require "../config.php";
-    try {
-    $connection = new PDO($dsn, $username, $password, $options);
-    // insert new user code will go here
-    } catch(PDOException $error) {
-    echo $sql . "<br>" . $error->getMessage();
+        require "../config.php";
+        try {
+            $connection = new PDO($dsn, $username, $password, $options);
+       
+        } catch(PDOException $error) {
+            echo $sql . "<br>" . $error->getMessage();
+        }
     }
-    }
+    $new_user = array(
+        "firstname" => $_POST['firstname'],
+        "lastname" => $_POST['lastname'],
+        "email" => $_POST['email'],
+        "age" => $_POST['age'],
+        "location" => $_POST['location']
+        );
 
 ?>
