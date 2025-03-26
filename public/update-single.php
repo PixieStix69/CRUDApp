371a1389b513345136b67983df76cc2123b20c68
Pixie <B00159841@mytudublin.ie>
2025-03-26 17:44:29 +0000

    Updated update-single.php

--- public/update-single.php
+++ public/update-single.php
@@ -2,10 +2,19 @@
 require "../common.php";
 
     if (isset($_GET['id'])) {
-        echo $_GET['id']; // for testing purposes
+        try {
+            require_once '../src/DBconnect.php';
+            $id = $_GET['id'];
+            $sql = "SELECT * FROM users WHERE id = :id";
+            $statement = $connection->prepare($sql);
+            $statement->bindValue(':id', $id);
+            $statement->execute();
+            $user = $statement->fetch(PDO::FETCH_ASSOC);
+        } catch(PDOException $error) {
+            echo $sql . "<br>" . $error->getMessage();
+        }
     } else {
         echo "Something went wrong!";
         exit;
     }
-
 ?>
