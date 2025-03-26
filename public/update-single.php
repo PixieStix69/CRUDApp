461f0e3e852df3ba81173d3fcfc123482f5b26f3
Pixie <B00159841@mytudublin.ie>
2025-03-26 17:57:24 +0000

    Updated update-single.php

--- public/update-single.php
+++ public/update-single.php
@@ -20,12 +20,18 @@ require "../common.php";
 ?>
 
 <?php require "templates/header.php"; ?>
+
     <h2>Edit a user</h2>
     <form method="post">
-<?php foreach ($user as $key => $value) : ?>
 
-<?php endforeach; ?>
-        <input type="submit" name="submit" value="Submit">
+        <?php foreach ($user as $key => $value) : ?>
+            <label for="<?php echo $key; ?>"><?php echo ucfirst($key); ?></label>
+            <input type="text" name="<?php echo $key; ?>" id="<?php echo $key;
+            ?>" value="<?php echo escape($value); ?>" <?php echo ($key === 'id' ?
+            'readonly' : null); ?>>
+        <?php endforeach; ?>
+            <input type="submit" name="submit" value="Submit">
     </form>
         <a href="index.php">Back to home</a>
+
 <?php require "templates/footer.php"; ?>
