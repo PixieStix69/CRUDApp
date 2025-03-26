4c5def40dbeea3a02b5b1efd3f30b67bbe33311d
Pixie <B00159841@mytudublin.ie>
2025-03-26 16:49:03 +0000

    Updated install.php

--- install.php
+++ install.php
@@ -1,5 +1,7 @@
 <?php 
 require "config.php";
 $connection = new PDO("mysql:host=$host", $username, $password, $options);
-
+$sql = file_get_contents("data/init.sql");
+$connection->exec($sql);
+Echo "DB setup";
 ?>
